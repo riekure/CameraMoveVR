b87b325d6c2aa652923f5c5e2ec51e4174097a74
riekure <s2darl0uvgia24@gmail.com>
2019-08-28 15:50:38 +0900

    update movePlane color

--- Assets/CameraMoveVR/Scripts/GazeUIController.cs
+++ Assets/CameraMoveVR/Scripts/GazeUIController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GazeUIController : MonoBehaviour
 {
     public GameObject mainCamera;
+    // 視点UIの対象パネル
+    public GameObject modePlane_1;
+
+    // 対象ボタンの色 0:通常/1:ヒット
+    public Material[] _materials;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +27,22 @@ public class GazeUIController : MonoBehaviour
         {
             // Sceneのみで線が見える
             Debug.DrawLine(ray.origin, hit.point, Color.black);
-        }
 
-        // 視点UIの対象かどうかをタグで判定
-        if (hit.collider.gameObject.tag == "VRUI")
-        {
-            Debug.Log("hit");
+            // 視点UIの対象かどうかをタグで判定
+            if (hit.collider.gameObject.tag == "VRUI")
+            {
+                Debug.Log("hit");
+                if (hit.collider.gameObject == modePlane_1)
+                {
+                    // ヒットの色
+                    modePlane_1.GetComponent<Renderer>().material = _materials[1];
+                }
+            }
+            else
+            {
+                // 通常の色
+                modePlane_1.GetComponent<Renderer>().material = _materials[0];
+            }
         }
     }
 }
