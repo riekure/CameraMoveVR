143448bf7337c7e9dee7b5471e80b4a107ead48e
riekure <s2darl0uvgia24@gmail.com>
2019-08-28 13:37:20 +0900

    create gyrocontroller and GazeUIController

---
+++ Assets/CameraMoveVR/Scripts/GazeUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeUIController : MonoBehaviour
+{
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Gaze UI Controller is started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Sceneのみで線が見える
+            Debug.DrawLine(ray.origin, hit.point, Color.black);
+        }
+
+        // 視点UIの対象かどうかをタグで判定
+        if (hit.collider.gameObject.tag == "VRUI")
+        {
+            Debug.Log("hit");
+        }
+    }
+}
---
+++ Assets/CameraMoveVR/Scripts/GyroController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroController : MonoBehaviour
+{
+    // キーボードでの操作用
+#if UNITY_EDITOR || UNITY_STANDALONE
+    private Vector3 rotate;
+#endif
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("started");
+#if UNITY_EDITOR || UNITY_STANDALONE
+        rotate = transform.rotation.eulerAngles;
+        Debug.Log("non-smartphone");
+#else
+        Input.gyro.enabled = true;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // PCはキーボード操作で視点変更、スマホはジャイロで視点変更
+#if UNITY_EDITOR || UNITY_STANDALONE
+        float speed = Time.deltaTime * 100.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotate.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotate.y += speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotate.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotate.x += speed;
+        }
+        transform.rotation = Quaternion.Euler(rotate);
+#else
+        Quaternion gattitude = Input.gyro.attitude;
+        gattitude.x *= -1;
+        gattitude.y *= -1;
+        transform.localRotation = Quaternion.Euler(90, 0, 0) * gattitude;
+#endif
+    }
+}
